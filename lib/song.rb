57eba8a7846c0379147f2a14c2cc4cae669334bf
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-19 20:21:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     rows = filename.split(" - ")
     song = self.new 
     song = rows[1].split(".mp3")
-    song.artist_name = rows[0].strip 
+    song.artist_name = rows[0].split  
     binding.pry 
     song 
   end 
